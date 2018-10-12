86b595cb7a5e53914204f52095cb618c6a2dbc2d
mtsani99 <mtsani99@gmail.com>
2018-10-12 14:19:27 +0300

    Included t1, t2 and t3 from w1.

---
+++ AgeAfter10Years/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AgeAfter10Years")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AgeAfter10Years")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ MyUniqueNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// This example code shows how you could implement the required main function for a 
+// Console UWP Application. You can replace all the code inside Main with your own custom code.
+
+// You should also change the Alias value in the AppExecutionAlias Extension in the 
+// Package.appxmanifest to a value that you define. To edit this file manually, right-click
+// it in Solution Explorer and select View Code, or open it with the XML Editor.
+
+namespace MyUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.WriteLine("My age after 10 years will be {AgeAfter10Years}. ");
+            int myAge = 19;
+            int AgeAfter10Years = myAge + 10;
+            Console.WriteLine("My last digits of my faculty number are {Last4Digits}.");
+            int Last4Digits = 1056;
+            double MyUniqueNumber = Math.Round((AgeAfter10Years * Last4Digits) / pi);
+        
+            Console.WriteLine("After 10 years I will be {AgeAfter10Years} years old. And my unique number is {MyUniqueNumber}. ");
+           
+        }
+    }
+}
